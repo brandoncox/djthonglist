4f4d02dc0fb064f483aedb411c47225b9fe7c2d2
Brandon Cox <bcox@redhat.com>
2016-03-28 11:04:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Djthonglist::Application.config.secret_key_base = 'db63e3bbe98819495959a322a6814155e0276a6637634b210fb18dbfc0101ad2496f9b9a75bdb8c313e830aad8bdb66d14cd94456ac92ffb504ff282f809c549'
